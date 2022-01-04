d1e09a336ac552c584ee6f36dda3a19b9f51e3da
Pawel Warmuth <pwarmuth@amazon.de>
2022-01-04 15:54:01 +0100

    Added csharp example base

---
+++ piwigo/src/Piwigo/PiwigoStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Amazon.CDK.AWS.RDS;
+
+namespace Piwigo
+{
+    public class PiwigoStack : Stack
+    {
+        internal PiwigoStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
