a99869a86e197472520c3ad3a47af88482f4ff56
PawelWarmuth <warmuth.pawel@gmail.com>
2022-01-10 22:11:47 +0100

    Added Networking Building on LoadBalancer

---
+++ piwigo/src/Piwigo/LoadBalancerSubStack.cs
@@ -0,0 +1,66 @@
+﻿using Amazon.CDK;
+using Amazon.CDK.AWS.AutoScaling;
+using Amazon.CDK.AWS.EC2;
+using Amazon.CDK.AWS.ElasticLoadBalancingV2;
+
+namespace Piwigo
+{
+    public class LoadBalancerProps
+    {
+        /// <summary>
+        /// The VPC the ALB resides in
+        /// </summary>
+        public Vpc Vpc { get;private set; }
+
+        /// <summary>
+        /// Security Group with all Ingress/Egress Rules for the ALB
+        /// </summary>
+        public SecurityGroup SecurityGroupLoadBalancer { get; private set; }
+
+        /// <summary>
+        /// The VPC SubnetGroup Name for the ALB to be hosted in
+        /// </summary>
+        public string LoadBalancerSubnetGroupName { get; private set; }
+
+        /// <summary>
+        /// The Server Instance where the Traffic from ALB is routed to
+        /// </summary>
+        public IInstance ServerInstance { get; private set; }
+    }
+
+    internal class LoadBalancerSubStack : NestedStack
+    {
+        public LoadBalancerSubStack(Constructs.Construct scope, string id, INestedStackProps props,LoadBalancerProps lbProps) : base(scope, id, props)
+        {
+            AutoScalingGroup asg = new AutoScalingGroup(scope,"pwigoAsg", new AutoScalingGroupProps
+            {
+                InstanceType = InstanceType.Of(InstanceClass.STANDARD5,InstanceSize.MICRO),
+                
+            });
+            var alb = new ApplicationLoadBalancer(scope, "piwigoLoadBalancer", new ApplicationLoadBalancerProps()
+            {
+                Vpc = lbProps.Vpc,
+                InternetFacing = true,
+                SecurityGroup = lbProps.SecurityGroupLoadBalancer,
+                VpcSubnets = new SubnetSelection { SubnetGroupName = lbProps.LoadBalancerSubnetGroupName },
+                IpAddressType = IpAddressType.IPV4
+            });
+
+            var albListener = alb.AddListener("httpsListener", new BaseApplicationListenerProps
+            {
+                Port = 443,
+                Open = true,
+                Protocol = ApplicationProtocol.HTTPS,
+                Certificates = new []
+                {
+                    new ListenerCertificate("ADD ARN HERE")
+                }
+            });
+
+            albListener.AddTargets("targets", new AddApplicationTargetsProps
+            {
+                Targets = new[] { asg }
+            });
+        }
+    }
+}
---
+++ piwigo/src/Piwigo/NetworkSubStack.cs
@@ -0,0 +1,53 @@
+﻿using Amazon.CDK;
+using Amazon.CDK.AWS.EC2;
+
+namespace Piwigo
+{
+    internal class NetworkSubStack : NestedStack
+    {
+        const string DATABASE_SUBNET_NAME = "dbSubnet";
+        const string SERVER_SUBNET_NAME = "serverSubnet";
+        const string ALB_SUBNET_NAME = "albSubnet";
+
+        public NetworkSubStack(Constructs.Construct scope, string id, INestedStackProps props = null) : base(scope, id, props)
+        { 
+            var vpc = new Vpc(scope, "piwigo-vpc", new VpcProps()
+            {
+                SubnetConfiguration = new[]
+                {
+                    new SubnetConfiguration()
+                    {
+                        CidrMask = 28,
+                        Name = DATABASE_SUBNET_NAME,
+                        SubnetType = SubnetType.PRIVATE
+                    },
+                    new SubnetConfiguration()
+                    {
+                        CidrMask = 28,
+                        Name = ALB_SUBNET_NAME,
+                        SubnetType = SubnetType.PUBLIC
+                    },
+                    new SubnetConfiguration()
+                    {
+                        CidrMask = 24,
+                        Name = SERVER_SUBNET_NAME,
+                        SubnetType = SubnetType.PRIVATE
+                    },
+                },
+                NatGatewaySubnets = new SubnetSelection()
+                {
+                    SubnetGroupName = SERVER_SUBNET_NAME
+                },
+                NatGateways = 1
+            });
+
+            var albSg = new SecurityGroup(scope, "alb-sg", new SecurityGroupProps() { Vpc = vpc });
+            var dbSg = new SecurityGroup(scope, "db-sg", new SecurityGroupProps() { Vpc = vpc });
+            var serverSg = new SecurityGroup(scope, "server-sg", new SecurityGroupProps() { Vpc = vpc });
+
+            serverSg.AddIngressRule(albSg, Port.Tcp(80));
+            albSg.AddIngressRule(Peer.AnyIpv4(), Port.Tcp(443));
+            dbSg.AddIngressRule(serverSg, Port.Tcp(3306));
+        }
+    }
+}
