f80d591dc1fd4f52733f160c225e3dc2e832a03d
Pawel Warmuth <warmuth.pawel@gmail.com>
2022-10-14 21:23:25 +0200

    Added piwigo_docker_simple

---
+++ pigallery2/lib/securityGroups.ts
@@ -0,0 +1,30 @@
+import { Stack } from 'aws-cdk-lib/core';
+import { Peer, Port, SecurityGroup, Vpc } from 'aws-cdk-lib/aws-ec2';
+import { Construct } from 'constructs';
+
+export class SecurityGroups extends Construct {
+
+    public readonly AlbSecurityGroup: SecurityGroup;
+    public readonly InstanceSecurityGroup: SecurityGroup
+    public readonly DatabaseSecurityGroup: SecurityGroup
+
+    constructor(parent: Stack, id: string, vpc: Vpc) {
+        super(parent, id);
+
+        this.AlbSecurityGroup = new SecurityGroup(parent, 'alb-securitygrp', {
+            vpc,
+            allowAllOutbound: true
+        });
+        this.AlbSecurityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(443), 'Inbound Traffic from anywhere to ALB');
+        this.InstanceSecurityGroup = new SecurityGroup(parent, 'webserver-securitygrp', {
+            vpc,
+            allowAllOutbound: true
+        });
+        this.InstanceSecurityGroup.addIngressRule(this.AlbSecurityGroup, Port.tcp(80), 'Allow Port 80 from ALB to Instance');
+        this.DatabaseSecurityGroup = new SecurityGroup(parent, 'db-securitygroup', {
+            vpc,
+            allowAllOutbound: true
+        });
+        this.DatabaseSecurityGroup.addIngressRule(this.InstanceSecurityGroup, Port.tcp(3306), 'Allow traffic from Webserver to Database');
+    }
+}
