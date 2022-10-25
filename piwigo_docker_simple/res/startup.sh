60e86f14cbd13d457a0e2faf9f846f267a88530a
Pawel Warmuth <warmuth.pawel@gmail.com>
2022-10-25 20:46:40 +0200

    Changed Shell File

--- piwigo_docker_simple/res/startup.sh
+++ piwigo_docker_simple/res/startup.sh
@@ -6,7 +6,7 @@ sudo chmod -v +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
-sudo wget https://github.com/Avarios/CDK-Examples/blob/main/piwigo_docker_simple/res/docker-compose.yaml -P /home/ec2-user/
+sudo wget https://raw.githubusercontent.com/Avarios/CDK-Examples/main/piwigo_docker_simple/res/docker-compose.yaml -P /home/ec2-user/
 sudo mkdir /home/ec2-user/piwigo
 chmod 777 /home/ec2-user/piwigo
 sudo docker-compose -f /home/ec2-user/docker-compose.yaml up -d
